1993c6ca6b6be2bb6c0ac8914f6f257cd0cc512d
Lucy Post <43582993+post2573@users.noreply.github.com>
2019-04-04 09:20:52 -0500

    Add files via upload

--- QuorPlayers.py
+++ QuorPlayers.py
@@ -18,42 +18,36 @@ class HumanPlayer():
     def __init__(self, game):
         self.game = game
         
-    def play(self, board):
-        game.display(game.getPlayerMatrix(), game.getPlayer2Matrix(), game.getHorizontalWallMatrix(), game.getVerticalWallMatrix(), game.getNumPlayerWalls(), game.getNumPlayer2Walls())
-        valid_pawn_moves = self.game.generateValidNorthPawnMoves(game.player, game.player2, game.h_walls, game.v_walls)
+    def play(self, game):
+        #display the board for the user so they can see options
+        self.game.display(self.game.getPlayerMatrix(), self.game.getPlayer2Matrix(), self.game.getHorizontalWallMatrix(), self.game.getVerticalWallMatrix(), self.game.getNumPlayerWalls(), self.game.getNumPlayer2Walls())
+        valid_pawn_moves = self.game.generateValidNorthPawnMoves(self.game.player, self.game.player2, self.game.h_walls, self.game.v_walls) 
         print("Valid Pawn Moves: ")
         for i in range(9):
             for j in range(9):
                 if valid_pawn_moves[i][j]:
                     print(str(i+1), str(j+1))
-                
-        print("Valid Horizontal Wall Moves: ")        
-        valid_horiz_moves = self.game.generateValidHorizWalls(game.player, game.player2, game.h_walls, game.v_walls)
-        for i in range(0,9):
-            for j in range(0,9):
-                if valid_horiz_moves[i][j]:
-                    print(str(i+1), str(j+1))
-                    
-        print("Valid Vertical Wall Moves: ")        
-        valid_vert_moves = self.game.generateValidVertWalls(game.player, game.player2, game.h_walls, game.v_walls)
-        for i in range(0,9):
-            for j in range(0,9):
-                if valid_vert_moves[i][j]:
-                    print(str(i+1), str(j+1))            
-                
+        valid_vertWalls = self.game.generateValidVertWalls(self.game.player, self.game.player2, self.game.h_walls, self.game.v_walls)
+        valid_horizWalls = self.game.generateValidHorizWalls(self.game.player, self.game.player2, self.game.h_walls, self.game.v_walls)
                 
         while True: 
+            print("while loop")
             # Python 3.x
             print("enter a move (e.g. a3 or c4v): ")
             a = input()
             # Python 2.x 
             # a = raw_input()
-            if(a.len() == 3):
-                x,y = [int(x) for x in a.split(' ')]
-                if valid_pawn_moves[x][y]:
-                    break
-                else:
-                    print('Invalid')
+            if(len(a) == 3):
+                if(a[2] == 'v'):
+                    result = self.game.isValidMove(a,valid_vertWalls)
+                elif(a[2] == 'h'):
+                    result = self.game.isValidMove(a,valid_horizWalls)
+            elif(len(a) == 2):
+                result = self.game.isValidMove(a,valid_pawn_moves)
+            if(result == True):
+                break
+            else:
+                print('Invalid')
             #need to add break down for a wall move
             #make a method in GameState that takes in single move?
         return a
